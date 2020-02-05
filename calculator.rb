fc25cf485301edbc897f7eb2b149f216d62644ff
Kaida Masaki <kaida@adadevelopersacademy.org>
2020-02-05 10:06:36 -0800

    Create calculator.rb

---
+++ calculator.rb
@@ -0,0 +1,11 @@
+puts "Welcome to the Calculator program!  Which operator would you like to use?"
+puts "1. add or +"
+puts "2. sub or -"
+puts "3. mul or *"
+puts "4. div or /"
+puts "Please choose one operator (name or symbol):"
+
+# TODO: Get user input!
+# TODO: Calculate!
+
+puts "Thank you for using the Calculator program!"
